18ec4405bc5f9cb977265a5eeefde73ab499bad4
lorirothermel <lori@rothermel1.com>
2024-09-04 12:39:17 -0400

    Not Working! Too many comments

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -7,19 +7,23 @@
 
 import SwiftUI
 import AVFAudio
+import PhotosUI
+
 
 
 struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer!
     @State private var animateImage = true
-    
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var bipImage = Image("clown")
     
     var body: some View {
         
         VStack {
+            
             Spacer()
             
-            Image("clown")
+            bipImage
                 .resizable()
                 .scaledToFit()
                 .scaleEffect(animateImage ? 1.0 : 0.9)
@@ -30,23 +34,102 @@ struct ContentView: View {
                         animateImage = true
                     }  // withAnimation
                 }  // .onTapGesture
-
+            
             
             
             Spacer()
             
-            Button(action: {
-                // TODO: - Button Action
-                
-            }, label: {
+            PhotosPicker(selection: $selectedPhoto, matching: .images, preferredItemEncoding: .automatic) {
                 Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
-                    
-            })
+            }  // PhotosPicker
+            
+            /*
+             .onChange(of: selectedPhoto) { _, newValue in
+             
+             Steps needed to get image
+             Get the data inside the PhotosPickerItem selectedPhoto.
+             Use the data to create a UIImage.
+             Use the UIImage to create an image.
+             Assign that image to bipImage.
+             
+             
+             First way to do this ->
+             
+             Task {
+             do {
+             if let data = try await newValue?.loadTransferable(type: Data.self) {
+             if let uiImage = UIImage(data: data) {
+             bipImage = Image(uiImage: uiImage)
+             }
+             }
+             } catch {
+             print("😡 ERROR: Loading failed! \(error.localizedDescription)")
+             }  // do...catch
+             
+             }  // Task
+             
+             
+             
+             Second way to do this ->
+             // Don't care about the error???
+             
+             Task {
+             guard let data = try? await newValue?.loadTransferable(type: Data.self) else {
+             print("😡 ERROR: Couldn't get data from loadTransferable!")
+             return
+             }  // guard let
+             
+             let uiImage = UIImage(data: data) ?? UIImage()
+             bipImage = Image(uiImage: uiImage)
+             // if an error is thrown after try? the curlies won't execute but error is ignored (nil).
+             }  // Task
+             
+             
+             // Third way
+             
+             Task {
+             if let data = try? await selectedPhoto?.loadTransferable(type: Data.self) {
+             let uiImage = UIImage(data: data) ?? UIImage()
+             bipImage = Image(uiImage: uiImage)
+             // If an error is thrown after try? curlies won't execute and error is ignored (nil)
+             }  // if let
+             }  // Task
+             
+             */
+            
+            .onChange(of: selectedPhoto) { _, newValue in
+                //                Task {
+                //                    do {
+                //                        if let data = try await newValue?.loadTransferable(type: Data.self) {
+                //                            if let uiImage = UIImage(data: data) {
+                //                                bipImage = Image(uiImage: uiImage)
+                //                            }
+                //                        }
+                //                    } catch {
+                //                        print("😡 ERROR: Loading failed! \(error.localizedDescription)")
+                //                    }  // do...catch
+                //
+                //                }  // Task
+                //            }  // .onChange
+                
+                Task {
+                    do {
+                        if let data = try await newValue?.loadTransferable(type: Data.self) {
+                            if let uiImage = UIImage(data: data) {
+                                bipImage = Image(uiImage: uiImage)
+                            }
+                        }  // if let data
+                    } catch {
+                        print("❗️ERROR: Loading failed - \(error.localizedDescription)")
+                    }  // do...catch
+                }  // Task
+            }
+            
             
             
             
         }  // VStack
-         
+        
     }  // some View
     
     func playSound(soundName: String) {

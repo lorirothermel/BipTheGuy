613256d2dbd109b8d8055dd3dc684b55c337c18a
lorirothermel <lori@rothermel1.com>
2024-09-04 11:05:32 -0400

    Chapter 3.2 Using scaleEffect, spring, and withAnimation to animate Clown image.

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,18 +6,67 @@
 //
 
 import SwiftUI
+import AVFAudio
+
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var animateImage = true
+    
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            Spacer()
+            
+            Image("clown")
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(animateImage ? 1.0 : 0.9)
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                    animateImage = false   // Will immediately shrink using .scaleEffect to 90% of size.
+                    withAnimation(.spring(response: 0.3, dampingFraction: 0.3)) {
+                        animateImage = true
+                    }  // withAnimation
+                }  // .onTapGesture
+
+            
+            
+            Spacer()
+            
+            Button(action: {
+                // TODO: - Button Action
+                
+            }, label: {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+                    
+            })
+            
+            
+            
+        }  // VStack
+         
+    }  // some View
+    
+    func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("❗️Could not read file named \(soundName)")
+            return
+        }  // guard let
+        
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("ERROR: \(error.localizedDescription). Creating audioPlayer.")
+        }  // do...catch
     }
-}
+    
+    
+    
+}  // ContentView
+
 
 #Preview {
     ContentView()
